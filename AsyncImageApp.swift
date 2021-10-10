5703ff194a61046e4ee083c0826ba485864104eb
Orhan Erbas <orhanerba@icloud.com>
2021-10-10 22:51:21 +0300

    Initial commit

---
+++ AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageApp.swift
+//  AsyncImage
+//
+//  Created by Orhan Erbas on 7.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
